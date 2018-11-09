88eff130dd9bd279ec07ebe135db49283c8582be
Travis Schilling <traviss@interknowlogy.com>
2018-11-09 09:12:24 -0800

    Updating the devices to rety making the calls to get the device info from the topology client.

---
+++ Source/Backend/SmartHotel.Devices/SmartHotel.Devices.Light/ManagementApiLimitReachedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartHotel.Devices.Light
+{
+	public class ManagementApiLimitReachedException : Exception
+	{
+		public ManagementApiLimitReachedException( string message )
+		: base( message )
+		{
+		}
+	}
+}
