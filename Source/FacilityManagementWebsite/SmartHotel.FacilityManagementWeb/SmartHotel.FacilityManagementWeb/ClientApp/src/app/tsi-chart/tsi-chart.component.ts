c48947f714309059ecf9e75c09333c38d6e4a3a3
Travis Schilling <traviss@interknowlogy.com>
2019-01-23 20:33:46 -0800

    updated chart to use the "old" GA/S SKU sdk calls in order to get aggregate data for the provided sensor ids

--- Source/FacilityManagementWebsite/SmartHotel.FacilityManagementWeb/SmartHotel.FacilityManagementWeb/ClientApp/src/app/tsi-chart/tsi-chart.component.ts
+++ Source/FacilityManagementWebsite/SmartHotel.FacilityManagementWeb/SmartHotel.FacilityManagementWeb/ClientApp/src/app/tsi-chart/tsi-chart.component.ts
@@ -14,76 +14,75 @@ export class TsiChartComponent implements OnInit, OnChanges {
   private tokenRetrieved = false;
   private token: string;
   private client: any;
-  private environmentFqdn = '6b193177-b605-4746-83a0-75fbbc885ebf.env.timeseries.azure.com';
+  // TODO: This needs to come from the environment
+  private environmentFqdn = 'bb6100ba-994c-44e3-84b2-01103a6e0b31.env.timeseries.azure.com';
   private resource = 'https://api.timeseries.azure.com/';
 
   constructor(private adalService: AdalService) {
   }
 
   private initializeAvgOccupancy() {
-    const filteredSensors = [];
-    filteredSensors.push(this.sensorIds[0]);
-    filteredSensors.push('e8d17c19-7dff-46a2-a3d0-2576e086ad9b'); // comment this line to see that it works with one sensor id
+    const filteredSensors: string[] = this.sensorIds;
+    // filteredSensors.push(this.sensorIds[0]);
+    // filteredSensors.push('e8d17c19-7dff-46a2-a3d0-2576e086ad9b'); // comment this line to see that it works with one sensor id
     // filteredSensors.push(this.sensorIds[1]);
 
     console.log(filteredSensors);
 
+    // TODO: Fix the name to chart or something other than chart2
     const lineChart = this.client.ux.LineChart(document.getElementById('chart2'));
-    const tsqExpressions = [];
-    const startDate = new Date('2019-01-14T00:00:00Z');
-    const endDate = new Date('2019-01-16T00:00:00Z');
-    tsqExpressions.push(new this.client.ux.TsqExpression({ timeSeriesId: filteredSensors }, // instance json
-      {
-        'Avg Occupancy': {
-          kind: 'numeric',
-          value: { tsx: '$event.Occupied.Double' },
-          filter: null,
-          aggregation: { tsx: 'avg($value)' }
-        }
-      }, // variable json
-      { from: startDate, to: endDate, bucketSize: '30m' }, // search span
-      '#60B9AE', // color
-      'Occupied')); // alias
-    tsqExpressions.push(new this.client.ux.TsqExpression({ timeSeriesId: filteredSensors }, // instance json
-      {
-        'Min Occupancy': {
-          kind: 'numeric',
-          value: { tsx: '$event.Occupied.Double' },
-          filter: null,
-          aggregation: { tsx: 'min($value)' }
-        }
-      }, // variable json
-      { from: startDate, to: endDate, bucketSize: '30m' }, // search span
-      'red', // color
-      'Occupied Min')); // alias
-    tsqExpressions.push(new this.client.ux.TsqExpression({ timeSeriesId: filteredSensors }, // instance json
-      {
-        'Max Occupancy': {
-          kind: 'numeric',
-          value: { tsx: '$event.Occupied.Double' },
-          filter: null,
-          aggregation: { tsx: 'max($value)' }
-        }
-      }, // variable json
-      { from: startDate, to: endDate, bucketSize: '30m' }, // search span
-      'green', // color
-      'Occupied Max')); // alias
 
+    const startDate = new Date('2019-01-23T00:00:00Z'); // Fix the date ranges to not be hardcoded. Should probably be a few days earlier
+    const endDate = new Date('2019-01-24T00:00:00Z'); // This should probably be end of "Today" whatever that day is.
 
-    const currentthis = this;
-    this.client.server.getTsqResults(this.token, this.environmentFqdn, tsqExpressions.map(function (ae) { return ae.toTsq(); }))
+    const predicate = this.buildPredicateString(filteredSensors);
+    console.log(predicate);
+    const aggregateExpressions = [];
+
+    aggregateExpressions.push(
+      new this.client.ux.AggregateExpression(
+        { predicateString: predicate }, // predicate
+        { property: 'Occupied', type: 'Double' }, // measure column
+        ['avg'], // measure type,
+        { from: startDate, to: endDate, bucketSize: '30m' },  // time range
+        null, // split by value, for you probably just null
+        '#60B9AE', // color
+        'Occupied Avg')  // display name
+    );
+
+    aggregateExpressions.push(
+      new this.client.ux.AggregateExpression(
+        { predicateString: predicate }, // predicate
+        { property: 'Occupied', type: 'Double' }, // measure column
+        ['min'], // measure type
+        { from: startDate, to: endDate, bucketSize: '30m' },  // time range
+        null, // split by value, for you probably just null
+        'Green', // color
+        'Occupied Min')  // display name
+    );
+
+    aggregateExpressions.push(
+      new this.client.ux.AggregateExpression(
+        { predicateString: predicate }, // predicate
+        { property: 'Occupied', type: 'Double' }, // measure column
+        ['max'], // measure type
+        { from: startDate, to: endDate, bucketSize: '30m' },  // time range
+        null, // split by value, for you probably just null
+        'Red', // color
+        'Occupied Max')  // display name
+    );
+
+    const currentThis = this;
+    this.client.server.getAggregates(this.token, this.environmentFqdn,
+      aggregateExpressions.map(function (ae) { return ae.toTsx(); }))
       .then(function (result) {
-        const transformedResult = currentthis.client.ux.transformTsqResultsForVisualization(result, tsqExpressions);
-        lineChart.render(transformedResult, { theme: 'light', grid: true, tooltip: true, legend: 'compact' }, tsqExpressions);
-
+        const transformedResult = currentThis.client.ux.transformAggregatesForVisualization(result, aggregateExpressions);
+        lineChart.render(transformedResult, { legend: 'compact' }, aggregateExpressions);
       });
-
-    return null;
   }
 
   ngOnInit() {
-
-    console.log('tsiui-chart-rendering create');
+    this.client = new TsiClient();
 
     this.token = this.adalService.getCachedToken(this.resource);
     if (!this.token) {
@@ -94,13 +93,10 @@ export class TsiChartComponent implements OnInit, OnChanges {
           this.tokenRetrieved = true;
           this.tryUpdateChart();
         });
+    } else {
+      this.tokenRetrieved = true;
+      this.tryUpdateChart();
     }
-    this.client = new TsiClient();
-
-    console.log(`tsiui-chart-rendering token loaded: ${this.token != null}`);
-
-
-
   }
 
   ngOnChanges() {
@@ -113,5 +109,10 @@ export class TsiChartComponent implements OnInit, OnChanges {
 
       this.initializeAvgOccupancy();
     }
+  }
+
+  private buildPredicateString(sensorIds: string[]) {
+    const result = sensorIds.join(`', '`);
+    return `SensorId in ('${result}')`;
   }
 }
